98bd7641eb97eab908f6f1ac6a09f8bf805c1a02
akbar <akbrg0872@gmail.com>
2025-03-27 19:41:48 -0700

    delete view (task)

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import TaskListView, TaskDetailView, TaskCreateView, TaskUpdateView
+from .views import TaskListView, TaskDetailView, TaskCreateView, TaskUpdateView, TaskDeleteView
 
 urlpatterns = [
     path('', TaskListView.as_view(), name='task_list'),
     path('<int:task_id>/', TaskDetailView.as_view(), name='task_detail'),
     path('create/', TaskCreateView.as_view(), name='task_create'),
     path('<int:task_id>/update/', TaskUpdateView.as_view(), name='task_update'),
+    path('<int:task_id>/delete/', TaskDeleteView.as_view(), name='task_delete'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,7 @@
 from django.urls import reverse_lazy
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from .models import Task
 from .forms import TaskForm
@@ -38,6 +38,15 @@ class TaskUpdateView(LoginRequiredMixin, UpdateView):
     model = Task
     form_class = TaskForm
     template_name = 'tasks/task_form.html'
+    success_url = reverse_lazy('task_list')
+
+    def get_object(self):
+        return get_object_or_404(Task, id=self.kwargs['task_id'], user=self.request.user)
+    
+
+class TaskDeleteView(LoginRequiredMixin, DeleteView):
+    model = Task
+    template_name = 'tasks/task_confirm_delete.html'
     success_url = reverse_lazy('task_list')
 
     def get_object(self):
