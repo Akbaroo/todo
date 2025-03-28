04566230fff16615ed32c75b896f6d131b200b55
akbar <akbrg0872@gmail.com>
2025-03-27 22:00:36 -0700

    update test view (app_namr)

--- tasks/tests/test_views.py
+++ tasks/tests/test_views.py
@@ -20,19 +20,19 @@ class TaskTest(TestCase):
 
     def test_task_list_view(self):
         self.client.login(username="testuser", password="testpass")
-        response = self.client.get(reverse("task_list"))
+        response = self.client.get(reverse("tasks:task_list"))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Test Task")
 
     def test_task_detail_view(self):
         self.client.login(username="testuser", password="testpass")
-        response = self.client.get(reverse("task_detail", args=[self.task.id]))
+        response = self.client.get(reverse("tasks:task_detail", args=[self.task.id]))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "This is a test task.")
     
     def test_create_task(self):
         self.client.login(username="testuser", password="testpass")
-        response = self.client.post(reverse("task_create"), {
+        response = self.client.post(reverse("tasks:task_create"), {
             "title": "New Task",
             "description": "New Task Description",
             "category": self.category.id,
@@ -43,7 +43,7 @@ class TaskTest(TestCase):
     
     def test_update_task(self):
         self.client.login(username="testuser", password="testpass")
-        response = self.client.post(reverse("task_update", args=[self.task.id]), {
+        response = self.client.post(reverse("tasks:task_update", args=[self.task.id]), {
             "title": "Updated Task",
             "description": "Updated Description",
             "category": self.category.id,
@@ -55,6 +55,6 @@ class TaskTest(TestCase):
     
     def test_delete_task(self):
         self.client.login(username="testuser", password="testpass")
-        response = self.client.post(reverse("task_delete", args=[self.task.id]))
+        response = self.client.post(reverse("tasks:task_delete", args=[self.task.id]))
         self.assertEqual(response.status_code, 302)
         self.assertFalse(Task.objects.filter(id=self.task.id).exists())
