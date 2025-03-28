63d15404b15ff80f659a96a3c087f2bc297a198f
akbar <akbrg0872@gmail.com>
2025-03-27 19:16:21 -0700

    task detail view

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import TaskListView
+from .views import TaskListView, TaskDetailView
 
 urlpatterns = [
     path('', TaskListView.as_view(), name='task_list'),
+    path('<int:task_id>/', TaskDetailView.as_view(), name='task_detail')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,8 @@
+from django.shortcuts import get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Task
-
 
 
 class TaskListView(LoginRequiredMixin, ListView):
@@ -12,3 +12,11 @@ class TaskListView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         return Task.objects.filter(user=self.request.user)
+
+
+class TaskDetailView(LoginRequiredMixin, DetailView):
+    model = Task
+    template_name = "tasks/task_detail.html"
+
+    def get_object(self):
+        return get_object_or_404(Task, pk=self.kwargs["task_id"], user=self.request.user)
