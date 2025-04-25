a61dc6f8863786677ec7806707e05f99dcc3f281
akbar <akbrg0872@gmail.com>
2025-04-25 15:56:28 -0700

    style(tasks.form): for template

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 from .models import Task
 
+
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ['title', 'description', 'category', 'status', 'due_date']
+        fields = ["title", "description", "category", "status", "due_date"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs["class"] = "form-control mb-3"
